bf121e8ad79b2a7cbdd5e5f1bc4023f1255971be
elrond <elrond@wsl.work>
2026-08-24 16:55:46 +0800

    feat: add contextual key hints to status bar

--- src/config.rs
+++ src/config.rs
@@ -184,10 +184,10 @@ impl ModuleConfig {
         widget_map: BTreeMap<String, Arc<dyn Widget>>,
     ) {
         let click_pos = match mouse {
-            Mouse::ScrollUp(_) => return,
-            Mouse::ScrollDown(_) => return,
-            Mouse::ScrollLeft(_) => return,
-            Mouse::ScrollRight(_) => return,
+            Mouse::ScrollUp(_)
+            | Mouse::ScrollDown(_)
+            | Mouse::ScrollLeft(_)
+            | Mouse::ScrollRight(_) => return,
             Mouse::LeftClick(_, y) => y,
             Mouse::RightClick(_, y) => y,
             Mouse::Hold(_, y) => y,
